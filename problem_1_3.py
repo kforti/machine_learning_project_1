e4eb045653c55efc7adae257130b48a39be22638
nhzero <bvalentino1@chewy.com>
2020-02-16 14:23:50 -0500

    update problem_1_3 to use MSE scoring and add discussion

--- problem_1_3.py
+++ problem_1_3.py
@@ -3,7 +3,7 @@ from sklearn import neighbors
 from sklearn import datasets
 
 from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 
 import numpy as np 
 
@@ -21,12 +21,12 @@ linear_regression_model.fit(diabetes_feature_data_train, diabetes_ground_truth_v
 ## Predict the values 
 linear_regression_predictions = linear_regression_model.predict(diabetes_feature_data_test);
 ## Calculate the error 
-linear_regression_r2_score = r2_score(diabetes_ground_truth_value_test, linear_regression_predictions)
-print('Accuracy without CV: %.2f'% linear_regression_r2_score)
+linear_regression_r2_score = mean_squared_error(diabetes_ground_truth_value_test, linear_regression_predictions)
+print('MSE without CV: %.2f'% linear_regression_r2_score)
 
 ## Use cross validation to calcualte the mse 
-scores = cross_val_score(linear_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=5)
-print("Accuracy with CV: %0.2f" % scores.mean())
+scores = cross_val_score(linear_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=10, scoring='neg_mean_squared_error')
+print("MSE with CV: %0.2f" % (scores.mean() * -1))
 
 
 # Part 2 - Ridge Regression
@@ -38,29 +38,29 @@ ridge_regression_model.fit(diabetes_feature_data_train, diabetes_ground_truth_va
 ## Predict the values 
 ridge_regression_predictions = ridge_regression_model.predict(diabetes_feature_data_test);
 ## Calculate the error 
-ridge_regression_r2_score = r2_score(diabetes_ground_truth_value_test, ridge_regression_predictions)
-print('Accuracy without CV: %.2f'% ridge_regression_r2_score)
+ridge_regression_r2_score = mean_squared_error(diabetes_ground_truth_value_test, ridge_regression_predictions)
+print('MSE without CV: %.2f'% ridge_regression_r2_score)
 
 ## Use cross validation to calcualte the mse 
-scores = cross_val_score(ridge_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=5)
-print("Accuracy with CV: %0.2f" % scores.mean())
+scores = cross_val_score(ridge_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=10, scoring='neg_mean_squared_error')
+print("MSE with CV: %0.2f" % (scores.mean() * -1))
 
 
 # Part 3 - Bayesian Ridge Regression 
 print("Bayesian Ridge Regression")
 ## Create and fit model 
-bayesian_ridge_regression_model = linear_model.Ridge()
+bayesian_ridge_regression_model = linear_model.BayesianRidge()
 bayesian_ridge_regression_model.fit(diabetes_feature_data_train, diabetes_ground_truth_value_train);
 
 ## Predict the values 
 bayesian_ridge_regression_predictions = ridge_regression_model.predict(diabetes_feature_data_test);
 ## Calculate the error 
-bayesian_ridge_regression_r2_score = r2_score(diabetes_ground_truth_value_test, bayesian_ridge_regression_predictions)
-print('Accuracy without CV: %.2f'% bayesian_ridge_regression_r2_score)
+bayesian_ridge_regression_r2_score = mean_squared_error(diabetes_ground_truth_value_test, bayesian_ridge_regression_predictions)
+print('MSE without CV: %.2f'% bayesian_ridge_regression_r2_score)
 
 ## Use cross validation to calcualte the mse 
-scores = cross_val_score(bayesian_ridge_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=5)
-print("Accuracy with CV: %0.2f" % scores.mean())
+scores = cross_val_score(bayesian_ridge_regression_model, diabetes_feature_data, diabetes_ground_truth_value, cv=10, scoring='neg_mean_squared_error')
+print("MSE with CV: %0.2f" % (scores.mean() * -1))
 
 # Part 4 - KNN 
 print("K-Nearest Neighbors")
@@ -71,9 +71,9 @@ knn_model.fit(diabetes_feature_data_train, diabetes_ground_truth_value_train);
 ## Predict the values 
 knn_predictions = knn_model.predict(diabetes_feature_data_test);
 ## Calculate the error 
-knn_r2_score = r2_score(diabetes_ground_truth_value_test, knn_predictions)
-print('Accuracy without CV: %.2f'% knn_r2_score)
+knn_r2_score = mean_squared_error(diabetes_ground_truth_value_test, knn_predictions)
+print('MSE without CV: %.2f'% knn_r2_score)
 
 ## Use cross validation to calcualte the mse 
-scores = cross_val_score(knn_model, diabetes_feature_data, diabetes_ground_truth_value, cv=5)
-print("Accuracy with CV: %0.2f" % scores.mean())
+scores = cross_val_score(knn_model, diabetes_feature_data, diabetes_ground_truth_value, cv=10, scoring='neg_mean_squared_error')
+print("MSE with CV: %0.2f" % (scores.mean() * -1))
